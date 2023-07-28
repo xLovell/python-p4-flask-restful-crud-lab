38f8588ec5fb2f2b710604bb1d2796af6e891bee
maddox earles <mdxear@gmail.com>
2023-07-28 15:13:04 -0600

    Complete lab

--- server/app.py
+++ server/app.py
@@ -46,6 +46,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in request.get_json():
+            setattr(plant, attr, request.get_json()[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+        response = make_response(plant_dict, 200)
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {"message": "no content"}
+        response = (response_dict, 204)
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
